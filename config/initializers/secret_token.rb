ca3530e2c75e05fee1fed72cacbab3c9a3767ec2
Ildar Kapkaev <kirs.box@gmail.com>
2012-01-10 15:38:51 +0400

    init new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orthodontic::Application.config.secret_token = '596fc0bf12f84c878d4a021cf41187cb840538ae0ed03739ddde2b9a95b6f9f5176c91581fc9086d95c3bcefc72b61f8af2c4c8136427cdb3528f27f3d691abd'
