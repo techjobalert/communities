c34fc6f034c800c0a48ef9f13bdd23b81cfde5da
Ildar Kapkaev <kirs.box@gmail.com>
2012-01-25 21:55:35 +0400

    fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,9 @@ Orthodontic::Application.configure do
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
+  # Sass caching off
+  config.sass.cache = false
+
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
