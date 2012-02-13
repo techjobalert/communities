cab981d13258e6f80cad1f9516ada98cc63769a2
orthodontics360 server <you@example.com>
2012-02-13 09:33:15 +0000

    nginx conf and redirect fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,25 +3,21 @@ class ApplicationController < ActionController::Base
   
   before_filter :authenticate
 
-
-	def after_sign_in_path_for(resource_or_scope)
-		puts resource_or_scope
-		
-	  # if session[:registered_from] == projects_register_path
-	  #   new_project_path
-	  # else
-	  #   account_index_path
-	  # end
-	end
+  def after_sign_in_path_for(resource_or_scope)	
+    # if session[:registered_from] == projects_register_path
+    #   new_project_path
+    # else
+    #   account_index_path
+    # end
+  end
 
   def after_sign_out_path_for(resource_or_scope)
-    root_path
+    #root_path
   end
   
   def authenticate
-    unless current_user.present?
-      redirect_to root_path
-    end
+    #unless current_user.present?
+    #  redirect_to root_path
+    #end
   end
-  
 end
