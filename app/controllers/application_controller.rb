15f11d18d10ab667c6522638fb079e359497267b
Ildar Kapkaev <kirs.box@gmail.com>
2012-02-14 16:04:02 +0400

    added basement permissions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
+
   def after_sign_out_path_for(resource)
     root_path
   end
@@ -8,4 +12,5 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     root_path
   end
+
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,4 @@
 class ItemsController < InheritedResources::Base
+  before_filter :authenticate_user!, :except => [:show, :index]
+  load_and_authorize_resource
 end
