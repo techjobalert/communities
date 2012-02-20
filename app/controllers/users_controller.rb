6769492635c332aab5f4adb16ce4c0e095f20223
Ildar Kapkaev <kirs.box@gmail.com>
2012-02-20 12:18:57 +0400

    disable abilityes for Item and User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!, :except => [:show]
-  load_and_authorize_resource
+  #load_and_authorize_resource
 
   def index
     @tab = params[:tab].present? ? params[:tab] : 'followers'
