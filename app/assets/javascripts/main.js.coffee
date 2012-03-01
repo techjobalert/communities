e2d8f6beb06266162360efc3dc660846a9149408
Ildar Kapkaev <kirs.box@gmail.com>
2012-03-01 14:52:41 +0400

    basement search autocomplite

--- app/assets/javascripts/main.js.coffee
+++ app/assets/javascripts/main.js.coffee
@@ -87,5 +87,14 @@ $ ->
     $(".popup-container").css("display","none");
     $(".l-settings-navigation").html("") if !$(this).hasClass "tab-settings"
 
-$("form[data-validate=true][data-remote=true]").live "change", ->
-  $(this).validate()
+  $("form[data-validate=true][data-remote=true]").live "change", ->
+    $(this).validate()
+
+  $("#qsearch").autocomplete(
+    source: "/search/qsearch"
+    minLength: 3
+    width: 100
+    select: (event, ui) ->
+      window.location.assign ui.item.url
+  ).data("autocomplete")._renderItem = (ul, item) ->
+    $("<li></li>").addClass("qsearch-item").data("item.autocomplete", item).append("<a href=\"" + item.url + "\">" + item.title + "</a>").appendTo ul
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,7 @@ class SearchController < ApplicationController
   def index
     @search_params = SearchParams.new(params)
     per_page = get_setting("show_search_results_per_page")
-    @search_results = @search_params.get_search_results.page(params[:page]).per(per_page)    
+    @search_results = @search_params.get_search_results.page(params[:page]).per(per_page)
   end
 
   def qsearch
@@ -19,9 +19,23 @@ class SearchController < ApplicationController
       {
         :title => item.title.truncate(40, :separator => ' '),
         :content => item.description.truncate(50, :separator => ' '),
-        :url => path_to_founded_item(item)
+        :url => polymorphic_path(item)
       }
     end
     render :json => @search_results
   end
+
+  private
+
+  def path_to_founded_item(item)
+    case
+      when item.instance_of?(Idea)
+        url_for([item.challenge, item])
+      when item.instance_of?(Challenge)
+        url_for([item])
+      else
+        polymorphic_path(item)
+    end
+  end
+
 end
