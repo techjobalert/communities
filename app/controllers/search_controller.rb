9bda9ed133583174b3ab151bdb15b2c68cd62942
Ildar Kapkaev <kirs.box@gmail.com>
2012-03-01 16:13:09 +0400

    qfix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -16,19 +16,11 @@ class SearchController < ApplicationController
 
     @search_results = @search_params.get_search_results || []
     @search_results.map! do |item|
-      if item.is_a? User do
-        {
-          :title => item.full_name.truncate(40, :separator => ' '),
-          :content => item.bio.truncate(50, :separator => ' '),
-          :url => polymorphic_path(item)
-        }
-      elsif item.is_a? Item do
-        {
-          :title => item.title.truncate(40, :separator => ' '),
-          :content => item.description.truncate(50, :separator => ' '),
-          :url => polymorphic_path(item)
-        }
-      end
+      {
+        :title => item.title.truncate(40, :separator => ' '),
+        :content => item.description.truncate(50, :separator => ' '),
+        :url => polymorphic_path(item)
+      }
     end
     render :json => @search_results
   end
