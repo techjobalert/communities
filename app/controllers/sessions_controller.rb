c294c18b2ccb9a67d8e1efa0ab0efacf7525f069
Oleg Shur <oshuravlev@provectus-it.com>
2012-03-06 13:25:53 +0400

    sign_in bug fix with redis

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,20 @@
 class SessionsController < Devise::SessionsController
   include SimpleCaptcha::ControllerHelpers
-  
-  def create
-    Rails.logger.info "===================== #{session.inspect} ============================="
 
-    session[:failure_counter].present? ? (session[:failure_counter] += 1) : session[:failure_counter] = 1
+  def create
+    failure_counter = $redis.get(session[:session_id])
+    failure_counter = (failure_counter.to_i || 0) + 1
+    $redis.set(session[:session_id], failure_counter)
     
-    if session[:failure_counter] <= 3 || simple_captcha_valid?      
+    if failure_counter <= 4 || simple_captcha_valid?
       resource = warden.authenticate!(auth_options)
-      set_flash_message(:notice, :signed_in) if is_navigational_format?      
+      set_flash_message(:notice, :signed_in) if is_navigational_format?
       sign_in(resource_name, resource)
-      #session[:failure_counter] = 0 if user_signed_in?
+      $redis.del(session[:session_id])
       redirect_to root_path
     else
       build_resource
       clean_up_passwords(resource)
-      flash.delete :recaptcha_error
       flash[:alert] = "There was an error with the captcha code below. Please re-enter the code."
       redirect_to "#{root_path}#sign-in"
     end
