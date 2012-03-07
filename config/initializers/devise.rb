b0e47fe3a3f58e821253ec22bdeffc392e530450
Oleg Shur <oshuravlev@provectus-it.com>
2012-03-07 12:30:12 +0400

    makeup

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -138,7 +138,7 @@ Devise.setup do |config|
   # :failed_attempts = Locks an account after a number of failed attempts to sign in.
   # :none            = No lock strategy. You should handle locking by yourself.
   # config.lock_strategy = :failed_attempts
-  config.lock_strategy = :none
+  # config.lock_strategy = :none
 
   # Defines which key will be used when locking and unlocking an account
   # config.unlock_keys = [ :email ]
@@ -148,7 +148,7 @@ Devise.setup do |config|
   # :time  = Re-enables login after a certain amount of time (see :unlock_in below)
   # :both  = Enables both strategies
   # :none  = No unlock strategy. You should handle unlocking by yourself.
-  config.unlock_strategy = :none
+  # config.unlock_strategy = :none
 
   # Number of authentication tries before locking an account if lock_strategy
   # is failed attempts.
