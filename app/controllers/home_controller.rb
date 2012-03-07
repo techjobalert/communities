6dd668865208e9f441c9ac9de3edc6a77165924a
Ildar Kapkaev <kirs.box@gmail.com>
2012-03-07 12:44:13 +0400

    qfix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @items = Item.published.order("created_at DESC").page params[:page]
+    @items = Item.state_is("published").order("created_at DESC").page params[:page]
   end
 
   def new_captcha
