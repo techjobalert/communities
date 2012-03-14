395a8441a5d548d52e04620d4b3a550bd63dcd4a
Ildar Kapkaev <kirs.box@gmail.com>
2012-03-14 13:09:16 +0400

    qfix

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -102,7 +102,7 @@ class ItemsController < InheritedResources::Base
 
   def users_search
     @item = Item.find(params[:item_id])
-    @doctors = User.search(params[:q], :without_ids => @item.contributor_ids, :with => {:role => "doctor"} )
+    @doctors = User.search(params[:q], :without_ids => [*@item.contributor_ids], :with => {:role => "doctor"} )
   end
 
   def qsearch
