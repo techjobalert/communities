6538dc201d0da8f5cba0f6e86098b518f7985d57
Oleg Shur <oshuravlev@provectus-it.com>
2012-04-12 21:18:03 +0400

    pjax

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,7 +14,7 @@ module ApplicationHelper
   def link_to_add_fields(name, f, association)
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-      render(association.to_s.singularize + "_fields", :f => builder)
+      render(association.to_s.singularize + "_fields", :f => builder, :removable => true)
     end
     link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
   end
@@ -31,7 +31,4 @@ module ApplicationHelper
     @devise_mapping ||= Devise.mappings[:user]
   end
 
-  def content_for_or_pjax(name, &block)
-    request.headers['X-PJAX'] ? capture(&block) : content_for(name, &block)
-  end
 end
