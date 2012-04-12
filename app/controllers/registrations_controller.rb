352b71750f3d8c624e7cccd307e571589a8d2f05
Oleg Shur <oshuravlev@provectus-it.com>
2012-04-12 14:37:05 +0400

    Create password change form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -27,7 +27,24 @@ def create
   end
 
   def update
-    super # no customization, simply call the devise implementation
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+
+    if resource.update_with_password(params[resource_name])
+      if is_navigational_format?
+        if resource.respond_to?(:pending_reconfirmation?) && resource.pending_reconfirmation?
+          flash_key = :update_needs_confirmation
+        end
+        set_flash_message :notice, flash_key || :updated
+      end
+      sign_in resource_name, resource, :bypass => true
+
+      #respond_with resource, :location => after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      flash[:error] = resource.errors.full_messages.first
+      #respond_with resource
+    end
+    redirect_to user_path(current_user)
   end
 
   protected
