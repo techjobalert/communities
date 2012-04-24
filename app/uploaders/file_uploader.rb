17ebc6ac8f018011bcb9beca3647ce4befeb6873
Ildar Kapkaev <kirs.box@gmail.com>
2012-04-24 13:14:28 +0400

    changes in debug info

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -170,9 +170,11 @@ class FileUploader < CarrierWave::Uploader::Base
   def is_video? f
     exts = %w(3gpp 3gp mpeg mpg mpe ogv mov webm flv mng asx asf wmv avi mp4 m4v).map!{|e| "."+ e }
     res = exts.member? File.extname(f.file)
+    p "is_presentation?(f) #{is_presentation?(f)}"
     if is_presentation?(f)
       cp = File.join(File.dirname(f.file), File.basename(f.file, '.*'))
       res = (File.exist? cp+".mov" or File.exist? cp+".wmv")
+      p "res #{res}"
     end
     res
   end
