721f0b397d73bf77dfd46015f653ee667258c404
Ildar Kapkaev <kirs.box@gmail.com>
2012-04-25 14:51:07 +0400

    qfix

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -47,17 +47,18 @@ class FileController < ApplicationController
       attachment.update_attribute("file_processing", nil)
       p_video = "/home/buildbot/video/video_storage/p_video/#{params[:filename]}"
 
+      record_file = File.open(p_video)
+      presenter_video = Attachment.new({
+        :file => record_file,
+        :user => current_user,
+        :attachment_type => "presentation_video"})
+
       #collect timing from subtitles
       # storing format "00:00:13,290;00:00:17,581" devider ";"
       _timing = []
       timing = %x[MP4Box #{p_video} -srt 3 -std].each_line{|l| _timing << l.split("-->")[1].strip() if l.include?("-->")}
       presenter_video.timing = timing.join(";")
 
-      record_file = File.open(p_video)
-      presenter_video = Attachment.new({
-        :file => record_file,
-        :user => current_user,
-        :attachment_type => "presentation_video"})
       attachment.item.attachments << presenter_video
     end
     render :nothing => true
