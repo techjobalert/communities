fcc9344926efba3691de05e481b06e51973cadba
Ildar Kapkaev <kirs.box@gmail.com>
2012-05-25 15:44:10 +0400

    fix

--- app/workers/power_point_convert.rb
+++ app/workers/power_point_convert.rb
@@ -3,13 +3,8 @@ require 'net/http'
 class PowerPointConvert
   @queue = :power_point_convert_queue
 
-  def self.perform file_ext, uuid_filename, model_id
-    if %w(.ppt .pptx).member? file_ext
-      uri = URI(REMOTE_WIN_PATH)
-    elsif file_ext == ".key"
-      uri = URI(REMOTE_MAC_PATH)
-    end
-
+  def self.perform uuid_filename, model_id
+    uri = URI(REMOTE_WIN_PATH)
     begin
       Net::HTTP.post_form(uri, 'file' => uuid_filename, 'id' => model_id)
     rescue Timeout::Error => e
