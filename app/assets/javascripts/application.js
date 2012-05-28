9b55ca1daf7a1a0897fe9e7780a78d8fe4a966e1
Ildar Kapkaev <kirs.box@gmail.com>
2012-05-29 00:46:29 +0400

    qfix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -152,6 +152,6 @@ function hmsToSeconds(t) {
   }
   return parseFloat(parseInt(ts[0])*60+parseInt(ts[1])*60+sec)
 }
-function secondsToMseconds(seconds){
-  return parseInt(hmsToSeconds(seconds).toString().replace(".",""))
+function hmsToMseconds(t){
+  return parseInt(hmsToSeconds(t).toString().replace(".",""))
 }
