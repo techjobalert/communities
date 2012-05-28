b70400e3797e6101bf8622e47fb502946de58cee
Ildar Kapkaev <kirs.box@gmail.com>
2012-05-29 00:06:36 +0400

    presentation video stop points input format for (flash video player) changed to ms

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -152,3 +152,6 @@ function hmsToSeconds(t) {
   }
   return parseFloat(parseInt(ts[0])*60+parseInt(ts[1])*60+sec)
 }
+function secondsToMseconds(seconds){
+  parseInt(hmsToSeconds(seconds).replace(".",""))
+}
