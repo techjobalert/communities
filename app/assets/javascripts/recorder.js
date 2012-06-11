a728bf5df8201ea36623d0abcbf9b4a6e3ef8b31
Ildar Kapkaev <kirs.box@gmail.com>
2012-06-11 12:46:26 +0400

    initialize for recorder js(fix)

--- app/assets/javascripts/recorder.js
+++ app/assets/javascripts/recorder.js
@@ -1,30 +1,29 @@
 var Recorder = window.Recorder = {
+  ins: undefined,
+  Recorder.playbackPoints:[],
+  playButton: $("#play_button"),
+  settings: {},
   initialize: function (params) {
-    var ins,
-        playbackPoints=[],
-        playButton = $("#play_button"),
-        recordFileName = params["fileName"],
-        resourceLink = params["resourceLink"],
-        requestUrl = params["requestUrl"],
-        timing = params["timing"],
-        videoId = params["videoId"];
+    _.each(params, function(v,k){Recorder.settings[k] = v});
+    Recorder.initInscription();
   },
-
   startRecord: function(){
     $("#start_record_button").attr("disabled","true");
     ins.record.start(recordFileName);
   },
   preparePoints:function(){
-    return _.map(playbackPoints, function(obj, num){ _.map(obj, function(o,n){ return secondsToHms(o)})})
+    return _.map(Recorder.playbackPoints, function(obj, num){ _.map(obj, function(o,n){ return secondsToHms(o)})})
   },
   mergeFunc: function(){
     $("#sync_button strong").text("Saving..");
-    var data = {  video_id: videoId,
-                  record_file_name: recordFileName,
-                  playback_points: preparePoints(),
-                  position: $("input[type=radio]:checked").val()};
+    var data = {
+      video_id: settings.videoId,
+      record_file_name: settings.recordFileName,
+      playback_points: preparePoints(),
+      position: $("input[type=radio]:checked").val()
+    };
     $.ajax({
-      url: requestUrl,
+      url: settings.requestUrl,
       type: "POST",
       dataType: "json",
       data: data,
@@ -35,10 +34,11 @@ var Recorder = window.Recorder = {
     });
   },
   initInscription: function(){
+    var ins;
     ins = Inscription.run({
       container: 'inscription',
       player: {
-          resource: resourceLink,
+          resource: settings.resourceLink,
           server: 'rtmp://89.209.76.243'
       }
     });
@@ -46,7 +46,7 @@ var Recorder = window.Recorder = {
     ins.on('load', function() {
       console.log('loaded');
       setTimeout(function() {
-        var points = Inscription.Point.read(timing);
+        var points = Inscription.Point.read(settings.timing);
         _.each(points, function(point) {
             ins.point.add(point);
             console.log('Added point: ' + point + ' ms...');
@@ -59,27 +59,27 @@ var Recorder = window.Recorder = {
     };
 
     var onPlay = function(points) {
-      var _s = _.size(playbackPoints);
+      var _s = _.size(Recorder.playbackPoints);
       var points = {
         stop: points[0],
         pause_duration: points[1]/1000,
-        start: _s ? playbackPoints[_s-1].stop : 0,
-        duration: points[0] - (_s ? playbackPoints[_s-1].stop : 0)
+        start: _s ? Recorder.playbackPoints[_s-1].stop : 0,
+        duration: points[0] - (_s ? Recorder.playbackPoints[_s-1].stop : 0)
       }
-      playbackPoints.push(points);
+      Recorder.playbackPoints.push(points);
     }
 
     var onStateChage = function(state) {
       console.log(state);
       if (state === "paused"){
-        playButton.removeAttr('disabled');
+        Recorder.playButton.removeAttr('disabled');
       }
       else if (state === "playing"){
-        playButton.attr("disabled","true");
+        Recorder.playButton.attr("disabled","true");
       }
       else if (state === "ready"){
         $("#sync_button").removeAttr('disabled');
-        playButton.attr("disabled","true");
+        Recorder.playButton.attr("disabled","true");
         ins.record.stop();
       }
     }
@@ -87,12 +87,13 @@ var Recorder = window.Recorder = {
     var onRecorderReady = function(status){
       if (status == true){
         $("#start_record_button").removeAttr('disabled');
-        playButton.removeAttr('disabled');
+        Recorder.playButton.removeAttr('disabled');
       }
     }
 
     ins.on('play',  onPlay);
     ins.on('stateChange', onStateChage);
     ins.on('recorderReady', onRecorderReady);
+    Recorder.ins = ins;
   }
 }
