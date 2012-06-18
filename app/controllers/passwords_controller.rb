d12b89f36dbad4ad0b5dafc2ce2cfe4a6f651290
ButuzGOL <stregub@selectica.com>
2012-06-18 15:41:50 +0300

    after update devise

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,7 +1,7 @@
 class PasswordsController < Devise::PasswordsController
   def create
-    #self.resource = resource_class.send_reset_password_instructions(resource_params) for 2.1.0
-    self.resource = resource_class.send_reset_password_instructions(params[resource_name]) # for 2.0.4
+    self.resource = resource_class.send_reset_password_instructions(resource_params) #for 2.1.0
+    #self.resource = resource_class.send_reset_password_instructions(params[resource_name]) # for 2.0.4
 
     if successfully_sent?(resource)
       respond_with({}, :location => root_path)
