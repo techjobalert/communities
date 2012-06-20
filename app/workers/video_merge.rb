3e08b495e4fe665a0143cd0ee168f1bd426f1605
Ildar Kapkaev <kirs.box@gmail.com>
2012-06-20 13:38:00 +0400

    empty audio if web record have not audio track(fix)

--- app/workers/video_merge.rb
+++ app/workers/video_merge.rb
@@ -25,7 +25,7 @@ class VideoMerge
       :presentV => p_att,
       :recordedV => r_att,
       :pos => self.add_position(),
-      :settings => '-map #{empty_audio_recorded ? 1 : 0}:0 -map 1:1 -async 1 -g 50 -acodec libfaac -ab 128k -ac 2 -vcodec libx264 -crf 22'
+      :settings => "-map #{empty_audio_recorded ? 1 : 0}:0 -map 1:1 -async 1 -g 50 -acodec libfaac -ab 128k -ac 2 -vcodec libx264 -crf 22"
       #-threads 1 -vcodec libvpx -acodec libvorbis -quality best -b:a 128k -b:v 1000k -qmin 10 -qmax 42 -maxrate 1500k -bufsize 1000k -vpre libvpx-720p
       # :metadata => '-title "OneWeekendInNYC"
       #               -author "Crazed Mule Productions, Inc."
