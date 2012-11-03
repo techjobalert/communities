7e58f41497dbeb3b5c46ca4ffd204a901aaedadc
V. Mogilevsky <triggerwoods91@gmail.com>
2012-11-03 16:08:08 +0400

    logging to debug

--- app/workers/send_processed_message.rb
+++ app/workers/send_processed_message.rb
@@ -2,7 +2,6 @@ class SendProcessedMessage
   @queue = :store_asset_notifications_queue
 
   def self.perform(item_id)
-  	Rails.logger.info "_____2______#{item_id}____________"
     NotifyMailer.send_processed_email_message(item_id).deliver
   end
 
