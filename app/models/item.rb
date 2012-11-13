c89d97db6c2e74970758bb87703f8fde1f7cab28
V. Mogilevsky <triggerwoods91@gmail.com>
2012-11-13 19:36:59 +0400

    show preview mode for videos

--- app/models/item.rb
+++ app/models/item.rb
@@ -30,6 +30,7 @@ class Item < ActiveRecord::Base
 
   # Handlers
   before_create  :add_to_contributors
+  around_update :update_preview, :if => :preview_length_changed?
 
   belongs_to  :user, :counter_cache => true, class_name: :User, inverse_of: :items
   belongs_to  :approved_by, class_name: :User, :foreign_key => "approved_by"
@@ -161,6 +162,25 @@ class Item < ActiveRecord::Base
 
   def can_get_pdf?(user)
     ((paid? and purchased?(user)) or (!paid? and published?) or self.user == user or user.admin?) and (attachment_type == "article")
+  end
+
+  private
+
+  def update_preview
+    if self.paid? && self.attachment_type.match(/video/)
+      last_attachment = self.attachments.where(:attachment_type => "#{self.attachment_type}_preview").last
+      if last_attachment && last_attachment.file_processing
+        self.errors.add(:preview_length,"can't change preview length before current preview file processing")
+        return false
+      else
+        if last_attachment
+          last_attachment.destroy 
+        end  
+        yield
+      end
+    else
+      yield
+    end
   end
 
 end
