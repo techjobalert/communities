c002a31b46300c9f4711e10331f75fbd8f635b09
V. Mogilevsky <triggerwoods91@gmail.com>
2012-11-20 21:27:43 +0400

    added google-api-client [2]

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,12 +1,14 @@
 class OmniauthCallbacksController <  Devise::OmniauthCallbacksController
   def google_oauth2
     # You need to implement the method below in your model
-
-    Rails.logger.info "----------#{request.inspect}--------"
     access_token = request.env["omniauth.auth"]
     if user_signed_in? && current_user
       if current_user.social_account_credential 
       	current_user.social_account_credential.update_attributes(:google_token => access_token.credentials.token, :google_user_id => access_token.uid)
+
+
+        debug_response = RestClient.get("https://www.google.com/m8/feeds/contacts/default/full&access_token=#{access_token.credentials.token}")
+
       else 
       	current_user.create_social_account_credential(:google_token => access_token.credentials.token, :google_user_id => access_token.uid)
      	end
