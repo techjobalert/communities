b689ddfb69191d57824a86a4322b4a9ef5bfda17
V. Mogilevsky <triggerwoods91@gmail.com>
2012-11-25 18:40:10 +0400

    fix bug ffmpeg

--- app/workers/create_preview.rb
+++ app/workers/create_preview.rb
@@ -1,26 +1,33 @@
 class CreatePreview
   @queue = :store_asset
 
-  def self.perform(item_id, to)
+  def self.perform(item_id, to, source_attachment=nil)
     #from 0 to #(seconds or pages)
     item = Item.find item_id
-    
+
     unless item.attachments.where(:attachment_type => "#{item.attachment_type}_preview").last
-      attachment = item.common_video
+      if source_attachment
+        attachment = Attachment.find(source_attachment)
+      else
+        attachment = item.common_video
+      end
       source = nil
       dest = nil
       hex = SecureRandom.hex
 
+      Rails.logger.debug "------------#{attachment.file}--------------"
       case item.attachment_type
       # when "pdf"
       #   # pdftk A=one.pdf B=two.pdf cat A1-7 B1-5 A8 output combined.pdf
       #   dest = File.join(File.dirname(attachment.file.path), "#{hex}-splited.pdf")
       #   system("pdftk A=#{attachment.file.path} cat A1-#{to} output #{dest}")
-      when "presenter_merged_video", "presentation_video", "video"       
+      when "presenter_merged_video", "presentation_video", "video"
         attachment.file.cache_stored_file! if !attachment.file.cached?
         source = attachment.file.path
         dest = File.join(File.dirname(source), "#{hex}-splited#{File.extname(source)}")
-        system("ffmpeg -y -i #{source} -ss 0 -t #{to} #{dest}")
+        Rails.logger.debug "________source___#{source}___________dest____#{dest}_______"
+        msg = system("ffmpeg -y -i #{source} -ss 0 -t #{to} #{dest}")
+        Rails.logger.debug "________msg___#{msg}________"
       end
 
       if dest
